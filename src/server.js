803b1f6229a819f607f65be7076bd068eb914742
Nino Uzelac <uzelac.nino@gmail.com>
2014-04-15 11:25:45 +0200

    syncanje kreiranja texta

--- src/server.js
+++ src/server.js
@@ -4,12 +4,7 @@
 		socket.on('createElement', function(data) {
 			console.log('createElement');
 			console.log(data);
-			socket.broadcast.emit('createElement', {
-				x: data.x,
-				y: data.y,
-				type: data.type,
-				info: data.info
-			})
+			socket.broadcast.emit('createElement', data);
 		});
 	});
 }).call(this);
